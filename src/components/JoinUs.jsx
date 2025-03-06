3e6a0ac890a97926843d3ca447add6c26ed441f5
emachizy <emachi2011@gmail.com>
2025-03-06 07:04:18 -0800

    updated

--- src/components/JoinUs.jsx
+++ src/components/JoinUs.jsx
@@ -6,6 +6,8 @@ import Typography from "@mui/material/Typography";
 import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 import Img from "../assets/Img/getInvolved.jpeg";
 
+import { MdVolunteerActivism } from "react-icons/md";
+
 export default function JoinUs() {
   const [expanded, setExpanded] = React.useState(null); // null means all are closed initially
 
@@ -19,7 +21,15 @@ export default function JoinUs() {
         <div>
           <img src={Img} alt="" />
         </div>
-        <div className="">
+        <div className="flex flex-col justify-center items-center">
+          <div className="text-[#DA7600] flex gap-1">
+            <MdVolunteerActivism />
+            <p className="text-sm pb-4">Join Us</p>
+          </div>
+          <h2 className="text-center text-3xl pb-4 font-bold">
+            We&apos;re always looking for talented individuals to join our team.
+          </h2>
+
           <Accordion
             expanded={expanded === "panel1"}
             onChange={handleChange("panel1")}
@@ -29,7 +39,7 @@ export default function JoinUs() {
               expandIcon={
                 <ArrowDownwardIcon
                   className={
-                    expanded === "panel1" ? "text-yellow-500" : "text-black"
+                    expanded === "panel1" ? "text-[#DA7600]" : "text-black"
                   }
                 />
               }
@@ -40,7 +50,7 @@ export default function JoinUs() {
               <Typography
                 component="span"
                 className={`text-lg font-semibold ${
-                  expanded === "panel1" ? "text-yellow-500" : "text-black"
+                  expanded === "panel1" ? "text-[#DA7600]" : "text-black"
                 }`}
               >
                 Recognition and Fulfillment
@@ -64,7 +74,7 @@ export default function JoinUs() {
               expandIcon={
                 <ArrowDownwardIcon
                   className={
-                    expanded === "panel2" ? "text-yellow-500" : "text-black"
+                    expanded === "panel2" ? "text-[#DA7600]" : "text-black"
                   }
                 />
               }
@@ -75,7 +85,7 @@ export default function JoinUs() {
               <Typography
                 component="span"
                 className={`text-lg font-semibold ${
-                  expanded === "panel2" ? "text-yellow-500" : "text-black"
+                  expanded === "panel2" ? "text-[#DA7600]" : "text-black"
                 }`}
               >
                 Why Join Us as a Volunteer?
@@ -99,7 +109,7 @@ export default function JoinUs() {
               expandIcon={
                 <ArrowDownwardIcon
                   className={
-                    expanded === "panel3" ? "text-yellow-500" : "text-black"
+                    expanded === "panel3" ? "text-[#DA7600]" : "text-black"
                   }
                 />
               }
@@ -109,8 +119,8 @@ export default function JoinUs() {
             >
               <Typography
                 component="span"
-                className={`text-lg font-semibold ${
-                  expanded === "panel3" ? "text-yellow-500" : "text-black"
+                className={`text-lg font-semibold transition-transform duration-500 ease-in-out ${
+                  expanded === "panel3" ? "text-[#DA7600]" : "text-black"
                 }`}
               >
                 Be Part of a Community
